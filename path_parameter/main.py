868370d675e9309c641ef81dfb70d63130c47a61
sazzad1779 <sazzad1779@gmail.com>
2024-02-03 10:16:41 +0600

    path parameter updated

--- path_parameter/main.py
+++ path_parameter/main.py
@@ -1,24 +1,43 @@
 
 
+
+
+
+from fastapi import FastAPI, Path , status
+from typing import Annotated 
+app = FastAPI()
+
+@app.get('/user/{id}',status_code=status.HTTP_200_OK)
+async def users(id:Annotated[int,Path(title="user id",ge=0,le=1000)]): #user id is integer which validated by 1 <= id <= 1000
+    return {"user id":id}
+
+@app.get('/user_name/{name}',status_code=status.HTTP_200_OK) #user name is string which validated by max_length==3 and min_length==100
+async def users(name:Annotated[str,Path(title="user name",min_length=0,max_length=100)]):
+    return {"user name":name}
+
+
 #path parameter should be named as decorator function's dynamic paramer.
 #if use alais then alais name and  decorator function's dynamic paramer must be same.
+@app.get('/user_address/{address}',status_code=status.HTTP_200_OK) #user address is string which validated by max_length==3 and min_length==1000
+async def users(addr:Annotated[str,Path(title="user address",min_length=0,max_length=1000 ,alias="address")]):
+    return {"Address ":addr}
 
-# from fastapi import FastAPI, status ,Path ,Query,Body 
-# from typing import Annotated
-# app = FastAPI()
 
-# # with out function parameter id data type 
-# @app.get('/user/{id}')
-# async def users(id):
-#     return id
+@app.get('/user_contact/{contact}',status_code=status.HTTP_200_OK) #user contact is string which validated by max_length==3 and min_length==11
+async def users(contact:Annotated[str,Path(title="user contact",min_length=0,max_length=11 ,alias="contact" ,description="Here is User contact")]):
+    return {"Contact ":contact}
 
-# # with out function parameter id data type 
-# @app.get('/user/{id}')
-# async def users(id:int):
-#     return id
-from fastapi import FastAPI, status ,Path ,Query,Body 
-from typing import Annotated
-app = FastAPI()
-@app.get('/user/{id}')
-async def users(id:Annotated[int,Path(...)]):
-    return {"user id":id}
+@app.get('/user_contact/{info}',status_code=status.HTTP_200_OK) #user contact is string which validated by max_length==3 and min_length==11
+async def users(info:Annotated[str,Path(title="user info",min_length=0,max_length=10 ,alias="info" ,description="Input User information",deprecated=True)]):
+    return {"Contact ":info}
+
+from enum import Enum
+class enum_type(str,Enum):
+    food = "vagitable"
+    meat = "beef"
+
+@app.get('/user_eat/{typ}')
+async def eats(eat:Annotated[enum_type,Path(...)]):
+    return eat
+
+
